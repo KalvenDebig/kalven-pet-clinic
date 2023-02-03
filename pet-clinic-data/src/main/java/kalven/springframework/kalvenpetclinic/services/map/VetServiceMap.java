6100179f4207cc1c4a63cd38372c9e4d63b1cbfc
Sijie Shen <shen.sij@northeastern.edu>
2023-02-02 19:12:02 -0500

    implemented map based services. closes #6

---
+++ pet-clinic-data/src/main/java/kalven/springframework/kalvenpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,34 @@
+package kalven.springframework.kalvenpetclinic.services.map;
+
+import kalven.springframework.kalvenpetclinic.model.Vet;
+import kalven.springframework.kalvenpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        super.save(object.getId(), object);
+        return object;
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
