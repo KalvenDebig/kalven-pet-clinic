062996b1e1938c42b6f2c5ede708c60b16f80928
Sijie Shen <shen.sij@northeastern.edu>
2023-02-04 17:50:07 -0500

    added specialty map service. closes #32.

---
+++ pet-clinic-data/src/main/java/kalven/springframework/kalvenpetclinic/services/SpecialtyService.java
@@ -0,0 +1,6 @@
+package kalven.springframework.kalvenpetclinic.services;
+
+import kalven.springframework.kalvenpetclinic.model.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+}
