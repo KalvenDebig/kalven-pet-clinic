4ae3899675da0bde620945fa5421c3e456f93fd1
Sijie Shen <shen.sij@northeastern.edu>
2023-02-04 16:58:28 -0500

    added specialty entity modified Vet entity. closes #28.

---
+++ pet-clinic-data/src/main/java/kalven/springframework/kalvenpetclinic/model/Specialty.java
@@ -0,0 +1,13 @@
+package kalven.springframework.kalvenpetclinic.model;
+
+public class Specialty extends BaseEntity {
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
