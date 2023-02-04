9633181a283992acd564f78a5dc3faee13021a6f
Sijie Shen <shen.sij@northeastern.edu>
2023-02-04 17:20:22 -0500

    added pettypes in bootstrap data. closes #33.

--- pet-clinic-web/src/main/java/kalven/springframework/kalvenpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/kalven/springframework/kalvenpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package kalven.springframework.kalvenpetclinic.bootstrap;
 
 import kalven.springframework.kalvenpetclinic.model.Owner;
+import kalven.springframework.kalvenpetclinic.model.PetType;
 import kalven.springframework.kalvenpetclinic.model.Vet;
 import kalven.springframework.kalvenpetclinic.services.OwnerService;
+import kalven.springframework.kalvenpetclinic.services.PetTypeService;
 import kalven.springframework.kalvenpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -11,15 +13,29 @@ import org.springframework.stereotype.Component;
 public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
     // no need for autowired
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        System.out.println("Start adding pet types");
+
+        PetType dog = new PetType();
+        dog.setName("dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
+
+
         System.out.println("Start loading owners");
         Owner kalven = new Owner();
         kalven.setFirstName("Kalven");
