747f06eb3052d75769ccf0b9222851bfa7ee245a
Sijie Shen <shen.sij@northeastern.edu>
2023-02-04 17:10:25 -0500

    added PetTypeServiceMap implemented PetType service interface. closes #30.

---
+++ pet-clinic-data/src/main/java/kalven/springframework/kalvenpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package kalven.springframework.kalvenpetclinic.services;
+
+import kalven.springframework.kalvenpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
