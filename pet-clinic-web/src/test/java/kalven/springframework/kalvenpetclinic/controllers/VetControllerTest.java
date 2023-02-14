370c11316e4779e45a83504022625162dba261a4
Sijie Shen <shen.sij@northeastern.edu>
2023-02-14 16:43:57 -0500

    modified vets list view. closes #69.

---
+++ pet-clinic-web/src/test/java/kalven/springframework/kalvenpetclinic/controllers/VetControllerTest.java
@@ -0,0 +1,16 @@
+package kalven.springframework.kalvenpetclinic.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @Project kalven-pet-clinic
+ * @Author kalvens on 2/14/23
+ */
+class VetControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
